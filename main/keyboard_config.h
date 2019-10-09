dbe56f66038d58bc7dce9b673a98d58eeeac737f
Anchit Chandra Sekhar <anchit.sekhar@gmail.com>
2019-10-08 23:42:18 -0400

    initial config for keyboard, minor bug fixes in keyboard scanning code.

--- main/keyboard_config.h
+++ main/keyboard_config.h
@@ -15,19 +15,19 @@
 #include "freertos/event_groups.h"
 
 #define MODULE_ID "LOLIN 32"
-#define GATTS_TAG "MK32 V3.0" // The device's name
+#define GATTS_TAG "Backpack Keyboard" // The device's name
 #define MAX_BT_DEVICENAME_LENGTH 40
 
-#define MASTER  // undefine if you are not flashing the main controller
-#define SPLIT_MASTER	 // undefine if keyboard is not split and master
+#define MASTER  // undefine if you are flashing the slave controller
+//#define SPLIT_MASTER	 // undefine if keyboard is not split and master
 //#define SLAVE	 // undefine if keyboard is master
 
-#define DEBOUNCE 7 //debounce time in ms
+#define DEBOUNCE 5 //debounce time in ms
 
 //Define matrix
-#define KEYPADS 2 // intended in order to create a Multiple keypad split boards
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 6 // For split keyboards, define columns for one side only.
+#define KEYPADS 1 // intended in order to create a Multiple keypad split boards
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17 // For split keyboards, define columns for one side only.
 
 #define LAYERS 3 // number of layers defined
 
@@ -36,14 +36,14 @@
 //#define ROW2COL
 
 //Encoder definitions
-#define R_ENCODER // undefine if no rotary encoder is used
+//#define R_ENCODER // undefine if no rotary encoder is used
 //#define R_ENCODER_SLAVE // undefine if no rotary encoder is used on slave pad
 #define ENCODER_A_PIN GPIO_NUM_32 // encoder phase A pin
 #define ENCODER_B_PIN GPIO_NUM_33// encoder phase B pin
 #define ENCODER_S_PIN GPIO_NUM_27// encoder switch pin
 
 //OLED Parameters
-#define OLED_ENABLE //undefine if no oled is used
+//#define OLED_ENABLE //undefine if no oled is used
 #define OLED_SDA_PIN GPIO_NUM_23
 #define OLED_SCL_PIN GPIO_NUM_22
 
@@ -52,7 +52,7 @@
  * use ADC1 only,  */
 
 #define BATT_STAT //define to enable battery monitoring
-#define BATT_PIN ADC1_CHANNEL_7 //gpio pin 35, refer to the esp32 before modifying
+#define BATT_PIN ADC1_CHANNEL_6 //gpio pin 34, refer to the esp32 before modifying
 
 //deep sleep parameters, mind that reconnecting after deep sleep might take a minute or two
 #define SLEEP_MINS 5 // undefine if you do not need deep sleep, otherwise define number of minutes for deepsleep

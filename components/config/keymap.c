620cb1c496dac526a029558124675f06f7a3ed56
danielfmo <danielfilipe.mo@gmail.com>
2020-07-25 10:47:14 +0200

    Add another example

--- components/config/keymap.c
+++ components/config/keymap.c
@@ -152,6 +152,17 @@ uint16_t _PLUGINS[MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTRL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_KP_MINUS, KC_KP_MINUS, KC_RALT, KC_RALT, KC_RGUI, KC_KP_ENTER, KC_RCTRL, KC_KP_PLUS, KC_KP_0, KC_KP_0, KC_KP_DOT}
 
 };
+// Example from https://github.com/SOGOR/MK32/blob/master/main/keymap.c
+// NOTE: For this keymap due to wiring constraints the the two last rows on the left are wired unconventionally
+    //  uint16_t _QWERTY[MATRIX_ROWS][KEYMAP_COLS]={
+    //         {KC_LCTL, KC_LGUI,  KC_LALT,    KC_F1,  KC_SPC, KC_SPC, KC_F6,  KC_SPC, KC_ENT, KC_NONE,    KC_RALT,    KC_RGUI,    KC_APP,     KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT },
+    //         {KC_LSFT, KC_LSFT,  KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_RSFT,    KC_MPRV,    KC_UP,      KC_MNXT },
+    //         {KC_CAPS, KC_A,     KC_S,       KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,       KC_SCLN,    KC_QUOT,    KC_BSLS,    KC_ENT,     KC_MSTP,    KC_MPLY,    KC_MSEL },
+    //         {KC_TAB,  KC_Q,     KC_W,       KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_DEL,     KC_END,     KC_PGDN },
+    //         {KC_1,    KC_2,     KC_3,       KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       KC_MINS,    KC_EQUAL,   KC_BSPC,    KC_BSPC,    KC_INS,     KC_HOME,    KC_PGUP },
+    //         {KC_ESC,  KC_GRV,   KC_F2,      KC_F3,  KC_F4,  KC_F5,  KC_F7,  KC_F8,  KC_F9,  FC_F10,     KC_F11,     KC_F12,     KC_VOLD,    KC_VOLU,    KC_PSCR,    KC_LSCR,    KC_BRK }
+    // };
+
 
 // Create an array that points to the various keymaps
 uint16_t (*default_layouts[])[MATRIX_ROWS][MATRIX_COLS] = {&_QWERTY, &_NUM, &_PLUGINS};
